d171c4e329a791b52add73fff93928d681dfd1da
Jesus Oroya <41575404+djoroya@users.noreply.github.com>
2024-10-11 03:01:21 +0200

    add

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,13 +1,54 @@
-import shutil,os
-# mkdir .repodeploy
-shutil.rmtree('.repo_deploy', ignore_errors=True)
-# mkdir .repo_deploy
-os.mkdir('.repo_deploy')
+import os
+import shutil
+from tools import get_repo_name, list_remote_branches, create_deploy_branch, clonar_deploy_branch,createsetup
+
+# ============== Main ===================
+
+folder_exists = os.path.exists('.repo_deploy')
+if not folder_exists:
+    os.makedirs('.repo_deploy')
+
+
+repo_url,name = get_repo_name()
+remote_branches = list_remote_branches(repo_url)
+print("Ramas remotas:", remote_branches)
+
+if not "deploy" in remote_branches:
+
+    print("La rama 'deploy' no existe en el repositorio remoto")
+    create_deploy_branch(repo_url)
+
+else:
+    print("La rama 'deploy' existe en el repositorio remoto")
+
+# combrobamos que exista .git
+    
+listdir = os.listdir(".repo_deploy")
+if not ".git" in listdir:
+    print("Clonando la rama 'deploy' del repositorio remoto")
+    #
+    clonar_deploy_branch(repo_url)
+    # remove .repo_deploy/*  remove all files in .repo_deploy except file initialized by .
+    shutil.rmtree('.repo_deploy/scripts', ignore_errors=True)
+    shutil.rmtree('.repo_deploy/src', ignore_errors=True)
+else:
+    print("La rama 'deploy' ya ha sido clonada")
+
+
+target_folder = os.path.join('src', 'dev', 'trial')
+copyfiles = ["README.md", "LICENSE", ".gitignore","requirements.txt"]
+for file in copyfiles:
+    shutil.copy(file, os.path.join(".repo_deploy", file))
+
 # copy src/* to .repo_deploy/name of the repo
+folder_name = os.path.join('.repo_deploy', name)
+if os.path.exists(folder_name):
+    shutil.rmtree(folder_name)
 
-stream = os.popen('git remote -v')
-output = stream.read()
-repo   = output.split("\n")[0].split("\t")[1].split(" ")[0]
+shutil.copytree(target_folder, folder_name)
 
-# clone the repo 
-os.system('git clone ' + repo + ' .repo_deploy/' + repo.split("/")[-1])
+os.chdir('.repo_deploy')
+createsetup(name)
+
+os.system('python setup.py sdist')
+os.system('python setup.py bdist_wheel')
---
+++ src/dev/main.py
@@ -0,0 +1,3 @@
+from trial.holamundo import holamundo
+
+holamundo()
